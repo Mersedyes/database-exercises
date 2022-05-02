fd4c7afa8b70bb355a6fc9d464550d44dee762a0
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-05-02 14:34:55 -0500

    moved lecture notes to tables_lec.sql and opened albums_migration.sql

---
+++ tables_lec.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+USE codeup_test_db;
+
+CREATE TABLE IF NOT EXISTS cats
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+SHOW TABLES;
+
+DESCRIBE cats;
+
+DROP TABLE IF EXISTS cats;
